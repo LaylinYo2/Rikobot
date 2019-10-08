9aefb19ae750b0077af23d1c0ff6b89f17b7134b
LaylinYo2 <56234214+LaylinYo2@users.noreply.github.com>
2019-10-08 17:50:44 +0300

    Add files via upload

---
+++ commands/zahando.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("You are not worthy.");
+    if(!args[0]) return message.channel.send("Specify an amount. | **Usage:** `>zahando <amount>`");
+    message.channel.bulkDelete(args[0]).then(() => {
+        message.channel.send(`Scraped away **${args[0]}** messages.`).then(msg => msg.delete(5000));
+    })
+}
+
+module.exports.help = {
+    name: "zahando"
+}
