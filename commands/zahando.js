e9f7ef5c406dd5e5d66b3d81272f48f1a33ee7f0
LaylinYo2 <56234214+LaylinYo2@users.noreply.github.com>
2019-10-08 18:15:28 +0300

    Update zahando.js

--- commands/zahando.js
+++ commands/zahando.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
         message.channel.send(`Scraped away **${args[0]}** messages.`).then(msg => msg.delete(5000));
         
                 let zaHando = new Discord.RichEmbed()
-        .setAuthor(message.guild.name, message.guild.displayAvatar)
+        .setAuthor(message.guild.name, message.guild.iconURL)
         .setDescription(`**Bulk Delete issued in ${message.channel}**\n**${args[0]} messages deleted.**`)
         .setTimestamp()
         .setColor("#e74c3c")
